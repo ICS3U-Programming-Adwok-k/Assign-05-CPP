f4ff3ebc4b57e0eafd6821430f638c4fe57e0340
Adwok-k <adwok.adiebo@stu.ocsb.ca>
2025-05-27 23:02:10 -0400

    Update surfaceAreaSquarePyramid.cpp

--- surfaceAreaSquarePyramid.cpp
+++ surfaceAreaSquarePyramid.cpp
@@ -28,14 +28,18 @@ float surfaceArea(float base, float height) {
 }
 
 int main() {
+    // This is the initial greeting that will
+    // be displayed as we run the program.
     std::cout << "Hello and welcome to Adwok's math equation." << std::endl;
 
     std::string baseString;
     std::string heightString;
 
+    // Get the base from the user as a string
     std::cout << "What is the base: ";
     std::cin >> baseString;
 
+    // Get the height from the user as a string
     std::cout << "What is the height: ";
     std::cin >> heightString;
 
@@ -44,10 +48,14 @@ int main() {
         float height;
         float calculatedArea;
 
+        // Convert the base to a float
         base = std::stof(baseString);
+
+        // Convert the height to a float
         height = std::stof(heightString);
 
         if (base <= 0 || height <= 0) {
+            // We have to ensure both base and height are both positive numbers
             if (base <= 0 && height <= 0) {
                 std::cout << "The base and height have to be"
                 " positive numbers." << std::endl;
@@ -61,11 +69,15 @@ int main() {
         } else {
             calculatedArea = surfaceArea(base, height);
             std::cout << "The surface area is: " << std::fixed
+            // Set the calculated area to 2 decimal places
             << std::setprecision(2) << calculatedArea << std::endl;
         }
     } catch (const std::invalid_argument& e) {
+        // If user enters an invalid input the code below will be executed
         std::cout << "Invalid input: Please enter a valid number." << std::endl;
     }
-
+    // Once all the code is done the code below will be
+    // executed because this is our finally
     std::cout << "Thank you for participating." << std::endl;
 }
+
